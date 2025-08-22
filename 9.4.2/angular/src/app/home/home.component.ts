6ad3c936d9694a9a7315ce3e60d4296633c235a2
Rajkumar Mali <rajkumarmali@Rajkumars.local>
2025-08-22 22:03:36 +0530

    edit Home Page

--- 9.4.2/angular/src/app/home/home.component.ts
+++ 9.4.2/angular/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Injector, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { CourseServicesServiceProxy, StudentServicesServiceProxy } from '@shared/service-proxies/service-proxies';
 
 @Component({
   templateUrl: './home.component.html',
@@ -8,7 +9,31 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent extends AppComponentBase {
-  constructor(injector: Injector) {
+  totalStudent:number =0;
+  totalCourse:number  = 0;
+  constructor(injector: Injector,private studentService:StudentServicesServiceProxy,private courseService:CourseServicesServiceProxy,private  changeDetector: ChangeDetectorRef) {
     super(injector);
+  }
+  ngOnInit():void{
+     this.studentService.getAllStudents().subscribe({
+        next:(res:any)=>{
+          this.totalStudent = res.length;
+          this.changeDetector.detectChanges();
+          console.log(this.totalStudent)
+        },
+        error:(err:any)=>{
+          console.log(err);
+        }
+     })
+     this.courseService.getAllCourses().subscribe({
+      next:(res:any)=>{
+        this.totalCourse = res.length;
+        this.changeDetector.detectChanges();
+        console.log(this.totalCourse)
+      },
+      error:(err:any)=>{
+        console.log(err);
+      }
+     })
   }
 }
