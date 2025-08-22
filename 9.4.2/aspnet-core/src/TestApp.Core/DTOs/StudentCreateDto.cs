c49cd4cf49b3e8d42a539ba834f94e6db7d4b23d
Rajkumar Mali <rajkumarmali@Rajkumars.local>
2025-08-22 15:06:49 +0530

    edit and delete student

--- 9.4.2/aspnet-core/src/TestApp.Core/DTOs/StudentCreateDto.cs
+++
@@ -1,9 +0,0 @@
-namespace TestApp.DTOs;
-
-public class StudentCreateDto
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-}
---
+++ 9.4.2/aspnet-core/src/TestApp.Core/DTOs/StudentDto.cs
@@ -0,0 +1,26 @@
+namespace TestApp.DTOs;
+
+public class StudentCreateDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
+public class GetStudentDto
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+public class StudentUpdateDto
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
